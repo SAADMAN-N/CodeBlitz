65cd34a6d33edc24110cc1f620f857722674ab41
Sharfuzzaman Noor Sadman <sadmansh@msu.edu>
2025-01-31 09:34:48 -0500

    Added navbar with Run and Submit buttons, styled buttons, included Font Awesome icons, adjusted spacing, made navbar transparent, increased button font weight and size, brought buttons closer, uncommented editor, and made Run button execute commands.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,65 @@
 import React from 'react';
 import Split from 'react-split';
-import CodeEditor from './components/CodeEditor';
 import './App.css';
 
 function App() {
   return (
-    <Split
-      sizes={[50, 50]}
-      minSize={100}
-      expandToMin={false}
-      gutterSize={10}
-      gutterAlign="center"
-      snapOffset={30}
-      dragInterval={1}
-      direction="horizontal"
-      cursor="col-resize"
-      className="split"
-    >
-      <div className="LeftPane">
-        {/* Left pane content can go here */}
+    <div className="App">
+      <div className="Navbar">
+        <button className="NavbarButton">
+          <i className="fas fa-play" style={{ color: 'white', marginRight: '10px' }}></i>
+          Run
+        </button>
+        <button className="NavbarButton">
+          <i className="fas fa-cloud" style={{ color: 'rgb(15, 187, 15)', marginRight: '10px' }}></i>
+          Submit
+        </button>
       </div>
-      <div className="CodeEditorContainer">
-        <CodeEditor />
-      </div>
-    </Split>
+      <Split
+        sizes={[50, 50]}
+        minSize={100}
+        gutterSize={5}  // Smaller gutter size for horizontal split
+        gutterAlign="center"
+        snapOffset={30}
+        dragInterval={1}
+        direction="horizontal"
+        cursor="col-resize"
+        className="split-horizontal"
+      >
+        <div className="LeftPane">
+          <div className="LeftHeader">
+            <span className="LeftIcon">{'</>'}</span> Questions
+          </div>
+          {/* Questions content can go here */}
+        </div>
+        <div className="RightPane">
+          <Split
+            sizes={[50, 50]}
+            minSize={100}
+            gutterSize={5}  // Smaller gutter size for vertical split
+            gutterAlign="center"
+            snapOffset={30}
+            dragInterval={1}
+            direction="vertical"
+            cursor="row-resize"
+            className="split-vertical"
+          >
+            <div className="EditorPane">
+              <div className="EditorHeader">
+                <span className="CodeIcon">{'</>'}</span> Code
+              </div>
+              {/* Editor content can go here */}
+            </div>
+            <div className="TestingPane">
+              <div className="TestingHeader">
+                <span className="TestingIcon">{'</>'}</span> Testing
+              </div>
+              {/* Testing content can go here */}
+            </div>
+          </Split>
+        </div>
+      </Split>
+    </div>
   );
 }
 
